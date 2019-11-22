134f4fcfe239f4121460c36454ecfed9ce27bbc8
Chase Tucker <tuck8106@gmail.com>
2019-11-22 10:18:13 -0800

    pushing to heroku to pull company financial data into db

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,15 @@
 -- TABLE FOR USERS AND PORTFOLIO
 -- 
 
--- DROP TABLE IF EXISTS  users, portfolios, companies, company_data;
-DROP TABLE IF EXISTS  portfolios;
+DROP TABLE IF EXISTS  users, portfolios, companies, company_data;
+-- DROP TABLE IF EXISTS  portfolios;
 
 -- DROP TABLE IF EXISTS users, portfolios;
 
--- CREATE TABLE users (
---   name VARCHAR(255),
---   id varchar(255) primary key
--- );
+CREATE TABLE users (
+  name VARCHAR(255),
+  id varchar(255) primary key
+);
 
 CREATE TABLE portfolios (
   id varchar(255) primary key,
@@ -18,24 +18,24 @@ CREATE TABLE portfolios (
   ticker varchar(255)
 );
 
--- CREATE TABLE companies (
---   id serial primary key,
---   ticker varchar(255),
---   name text,
---   description text,
---   industry text,
---   url text,
---   data_id int
--- );
+CREATE TABLE companies (
+  id serial primary key,
+  ticker varchar(255),
+  name text,
+  description text,
+  industry text,
+  url text,
+  data_id int
+);
 
--- CREATE TABLE company_data (
---   ticker varchar(255) primary key,
---   price varchar(255),
---   pe float,
---   pb float,
---   peg float,
---   profit_margin varchar(255),
---   market_cap varchar(255)
--- );
+CREATE TABLE company_data (
+  ticker varchar(255) primary key,
+  price varchar(255),
+  pe float,
+  pb float,
+  peg float,
+  profit_margin varchar(255),
+  market_cap varchar(255)
+);
 
 -- ADD/DELETE/EDIT PORTFOLIO
